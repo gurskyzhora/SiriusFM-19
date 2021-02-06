9931b69db6ddc294c2b29abb1aaf2ed5c9da61ac
Georgy Gursky <gursky.zhora@yandex.ru>
2021-02-06 11:30:50 +0300

    first commit

---
+++ DiffusionCEV.h
@@ -0,0 +1,30 @@
+#include <cmath>
+#include <fstream>
+#include <iostream>
+
+namespace SiriusFM{
+    class DiffusionCEV
+    {
+        private:
+            double const const_mu;
+            double const const_sigma;
+            double const beta;
+        public:
+            double mu(double St, double t){
+                return const_mu*St;
+            }
+            double sigma(double St, double t){
+                if(beta<0){return 0;}
+                if(const_sigma<=0){return 0;}
+                if(beta<0){return 0;}
+                return const_sigma*pow(St, beta);
+            }
+        DiffusionCEV(double const_m, double const_s, double const_beta)
+        :const_mu(const_m),
+         const_sigma(const_s),
+         beta(const_beta)
+        {
+        if(const_sigma<=0 || beta < 0){throw std::invalid_argument("Bad arguments");}
+        }
+    };
+}
